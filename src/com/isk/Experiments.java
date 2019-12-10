d500b067dd8adb426941d0bfe9250fff46d8dc3c
Sebastian Wawro <user6917@gmail.com>
2019-12-10 17:56:57 +0100

    Other experiments

--- src/com/isk/Experiments.java
+++ src/com/isk/Experiments.java
@@ -7,7 +7,22 @@ public class Experiments {
     private static boolean verbosity = false;
     private static int repeatCount = 20;
 
-    public static void displayCurrentResult(SurvivedChromosomeData survivedChromosomeData) {
+    private static String crossoverTypeToString(int type) {
+        switch(type) {
+            case 0:
+                return "One Point Crossover";
+            case 1:
+                return "Two Point Crossover";
+            case 2:
+                return "Uniform Crossover";
+            case 3:
+                return "Roulette Crossover";
+            default:
+                return "WTF Crossover";
+        }
+    }
+
+    public static void displayCurrentResult(SurvivedChromosomeData survivedChromosomeData, SettingsForGA settingsForGA) {
         List<PossibleItem> possibleItems = AppMemory.getInstance().getPossibleItems();
         System.out.println("Fitness: " + survivedChromosomeData.getFitness());
         System.out.println("Genes: ");
@@ -26,6 +41,11 @@ public class Experiments {
         }
         System.out.println("Total size: " + totalSize);
         System.out.println("Total price: " + totalPrice);
+        System.out.println("Used settings: ");
+        System.out.println("Population size: " + settingsForGA.populationSize);
+        System.out.println("Crossover type: " + crossoverTypeToString(settingsForGA.crossoverType));
+        System.out.println("Crossover chance: " + settingsForGA.crossoverChance);
+        System.out.println("Mutation chance: " + settingsForGA.mutationChance);
     }
 
     public static double countItemsSize(SurvivedChromosomeData survivedChromosomeData) {
@@ -67,12 +87,105 @@ public class Experiments {
         SettingsForGA settingsForGA = new SettingsForGA();
         for (int rp=0; rp<repeatCount*8; rp++) {
             SurvivedChromosomeData score = GACurveFitX.performGA(settingsForGA);
+            if (score == null) continue;
             double totalPrice = countItemsPrice(score);
             double totalSize = countItemsSize(score);
             double execTime = score.getExecTime();
             int isOK = isOK(totalSize)?1:0;
-            stringBuilder.append(totalPrice+"\t"+totalSize+"\t"+execTime+"\t"+isOK);
-            if (verbosity) displayCurrentResult(score);
+            stringBuilder.append(totalPrice+"\t"+totalSize+"\t"+execTime+"\t"+isOK+"\r\n");
+            if (verbosity) displayCurrentResult(score, settingsForGA);
+        }
+        String result = stringBuilder.toString();
+        FileManager.writeFile(fileName, result);
+        return scoresList;
+    }
+
+    public static List<SurvivedChromosomeData> testPopulation(String fileName) throws Exception {
+        List<SurvivedChromosomeData> scoresList = new ArrayList<>();
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("Wartość spakowanych itemków\tWaga spakowanych itemków\tCzas pakowania\tCzy poprawne\tRozmiar populacji\r\n");
+        SettingsForGA settingsForGA = new SettingsForGA();
+        for (int iPop = 5; iPop <= 500; iPop +=5) {
+            settingsForGA.populationSize=iPop;
+            for (int rp=0; rp<repeatCount; rp++) {
+                SurvivedChromosomeData score = GACurveFitX.performGA(settingsForGA);
+                if (score == null) continue;
+                double totalPrice = countItemsPrice(score);
+                double totalSize = countItemsSize(score);
+                double execTime = score.getExecTime();
+                int isOK = isOK(totalSize)?1:0;
+                stringBuilder.append(totalPrice+"\t"+totalSize+"\t"+execTime+"\t"+isOK+"\t"+iPop+"\r\n");
+                if (verbosity) displayCurrentResult(score, settingsForGA);
+            }
+        }
+        String result = stringBuilder.toString();
+        FileManager.writeFile(fileName, result);
+        return scoresList;
+    }
+
+    public static List<SurvivedChromosomeData> testDifferentCrossovers(String fileName) throws Exception {
+        List<SurvivedChromosomeData> scoresList = new ArrayList<>();
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("Wartość spakowanych itemków\tWaga spakowanych itemków\tCzas pakowania\tCzy poprawne\tTyp crossovera\r\n");
+        SettingsForGA settingsForGA = new SettingsForGA();
+        for (int iType = 0; iType <= 3; iType++) {
+            settingsForGA.crossoverType = iType;
+            for (int rp=0; rp<repeatCount; rp++) {
+                SurvivedChromosomeData score = GACurveFitX.performGA(settingsForGA);
+                if (score == null) continue;
+                double totalPrice = countItemsPrice(score);
+                double totalSize = countItemsSize(score);
+                double execTime = score.getExecTime();
+                int isOK = isOK(totalSize)?1:0;
+                stringBuilder.append(totalPrice+"\t"+totalSize+"\t"+execTime+"\t"+isOK+"\t"+crossoverTypeToString(iType)+"\r\n");
+                if (verbosity) displayCurrentResult(score, settingsForGA);
+            }
+        }
+        String result = stringBuilder.toString();
+        FileManager.writeFile(fileName, result);
+        return scoresList;
+    }
+
+    public static List<SurvivedChromosomeData> testCrossoverChance(String fileName) throws Exception {
+        List<SurvivedChromosomeData> scoresList = new ArrayList<>();
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("Wartość spakowanych itemków\tWaga spakowanych itemków\tCzas pakowania\tCzy poprawne\tSzansa crossovera\r\n");
+        SettingsForGA settingsForGA = new SettingsForGA();
+        for (double iCrossChance = 0.05f; iCrossChance < 1.0f; iCrossChance += 0.05f) {
+            settingsForGA.crossoverChance = iCrossChance;
+            for (int rp=0; rp<repeatCount; rp++) {
+                SurvivedChromosomeData score = GACurveFitX.performGA(settingsForGA);
+                if (score == null) continue;
+                double totalPrice = countItemsPrice(score);
+                double totalSize = countItemsSize(score);
+                double execTime = score.getExecTime();
+                int isOK = isOK(totalSize)?1:0;
+                stringBuilder.append(totalPrice+"\t"+totalSize+"\t"+execTime+"\t"+isOK+"\t"+iCrossChance+"\r\n");
+                if (verbosity) displayCurrentResult(score, settingsForGA);
+            }
+        }
+        String result = stringBuilder.toString();
+        FileManager.writeFile(fileName, result);
+        return scoresList;
+    }
+
+    public static List<SurvivedChromosomeData> testMutationChance(String fileName) throws Exception {
+        List<SurvivedChromosomeData> scoresList = new ArrayList<>();
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("Wartość spakowanych itemków\tWaga spakowanych itemków\tCzas pakowania\tCzy poprawne\tSzansa mutacji\r\n");
+        SettingsForGA settingsForGA = new SettingsForGA();
+        for (double iMutChance = 0.05f; iMutChance < 1.0f; iMutChance += 0.01f) {
+            settingsForGA.mutationChance=iMutChance;
+            for (int rp=0; rp<repeatCount; rp++) {
+                SurvivedChromosomeData score = GACurveFitX.performGA(settingsForGA);
+                if (score == null) continue;
+                double totalPrice = countItemsPrice(score);
+                double totalSize = countItemsSize(score);
+                double execTime = score.getExecTime();
+                int isOK = isOK(totalSize)?1:0;
+                stringBuilder.append(totalPrice+"\t"+totalSize+"\t"+execTime+"\t"+isOK+"\t"+iMutChance+"\r\n");
+                if (verbosity) displayCurrentResult(score, settingsForGA);
+            }
         }
         String result = stringBuilder.toString();
         FileManager.writeFile(fileName, result);
